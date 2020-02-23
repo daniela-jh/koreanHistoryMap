67e51911d40e54e1972ded72b3784aedc213e67c
Junghwa Bae <51525511+daniela-jh@users.noreply.github.com>
2020-02-23 17:29:26 +0900

    Search Upgrade
    -검색어가 없는 경우 submit 제한, alert 노출
    -검색어 앞뒤로 공백 있는 경우 제외하고 검색
    - 검색 결과 화면에서 검색어에 강조 표시

--- search_app/views.py
+++ search_app/views.py
@@ -7,5 +7,13 @@ def searchResult(request):
     query = None
     if 'q' in request.GET:
         query = request.GET.get('q')
-        contents = Content.objects.all().filter(Q(locationName__contains=query) | Q(locationText1__contains=query) | Q(locationText2__contains=query) | Q(locationText3__contains=query) | Q(locationText4__contains=query))
-        return render(request, 'search_app/search.html', {'query':query, 'contents':contents})
+        queryStrip = query.strip()
+        contents = Content.objects.all().filter(Q(locationName__contains=queryStrip) | Q(locationText1__contains=queryStrip) | Q(locationText2__contains=queryStrip) | Q(locationText3__contains=queryStrip) | Q(locationText4__contains=queryStrip))
+        for content in contents:
+            content.locationName = content.locationName.replace(queryStrip, '<strong>' + queryStrip + '</strong>');
+            content.locationText1 = content.locationText1.replace(queryStrip, '<strong>' + queryStrip + '</strong>');
+            content.locationText2 = content.locationText2.replace(queryStrip, '<strong>' + queryStrip + '</strong>');
+            content.locationText3 = content.locationText3.replace(queryStrip, '<strong>' + queryStrip + '</strong>');
+            content.locationText4 = content.locationText4.replace(queryStrip, '<strong>' + queryStrip + '</strong>')
+
+        return render(request, 'search_app/search.html', {'query':queryStrip, 'contents':contents})
