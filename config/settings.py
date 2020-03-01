b2e59b8fe8c6d102f6b25748e4b5bbb7d8a4b8bc
Junghwa Bae <51525511+daniela-jh@users.noreply.github.com>
2020-03-01 14:15:52 +0900

    add host

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,10 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'historymap.kr',
+    'www.historymap.kr',
+]
 
 
 # Application definition
