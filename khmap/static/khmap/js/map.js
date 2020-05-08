70497a4e7fa5d528eae8ff7c2f66e646712ce28c
Junghwa Bae <51525511+daniela-jh@users.noreply.github.com>
2020-05-08 15:28:22 +0900

    common.css type 추가

--- khmap/static/khmap/js/map.js
+++ khmap/static/khmap/js/map.js
@@ -6,11 +6,16 @@ function getMapSize(screen_width, screen_height) {
     var size = new naver.maps.Size(screen_width, screen_height - headerHeight - footerHeight);
     return size;
 };
+var sw = new naver.maps.LatLng(21.0095645, 107.8205401),
+    ne = new naver.maps.LatLng(46.9380714,134.1204317);
+
 var mapOptions = {
     center: new naver.maps.LatLng(38.0000000, 126.9783882),
     zoom: 6,
     minZoom: 3,
     maxZoom: 9,
+    maxBounds: new naver.maps.LatLngBounds(sw, ne),
+    tileSpare: 10,
 };
 
 var map = new naver.maps.Map('map', mapOptions);
